71cf31c8cd81c9b2a4974a7a0c21139c7faa9308
ddddd <test>
2019-10-30 21:38:57 +0800

    新增读取excel方法

--- case/login.py
+++ case/login.py
@@ -9,6 +9,8 @@ from common.operate_page import BasePage
 class Login(unittest.TestCase):
     def setUp(self):
         self.d = webdriver.Firefox()
+        url = readconfig.url
+        self.d.get(url)
     def tearDown(self):
         userName = self.d.find_element(By.XPATH, '//*[@class = "dropdown-toggle"]').text
         self.assertEqual(userName, u'晨曦')
@@ -16,22 +18,13 @@ class Login(unittest.TestCase):
     def test_login1(self):
         inputPhone = readconfig.inputPhone
         inputPsw = readconfig.inputPsw
-        url = readconfig.url
-        self.d.get(url)
         B = BasePage(self.d)
-        # self.d.find_element(By.XPATH,'/html/body/nav/div/div[2]/ul/li[2]/a').click()
         B.find_web_element(By.XPATH,'/html/body/nav/div/div[2]/ul/li[2]/a').click()
-        # self.d.find_element(By.XPATH,'//*[@id="login_span_type_pwd"]').click()
         B.find_web_element(By.XPATH,'//*[@id="login_span_type_pwd"]').click()
-        # self.d.find_element(By.XPATH,'// *[@id = "login_id_input_phone"]').send_keys(inputPhone)
         B.find_web_element(By.XPATH,'// *[@id = "login_id_input_phone"]').send_keys(inputPhone)
-        # self.d.find_element(By.XPATH,'//*[@id ="login_id_input_password"]').send_keys(inputPsw)
         B.find_web_element(By.XPATH,'//*[@id ="login_id_input_password"]').send_keys(inputPsw)
-        # self.d.find_element(By.XPATH,'//*[@id="login_id_login"]').click()
         B.find_web_element(By.XPATH,'//*[@id="login_id_login"]').click()
         sleep(3)
-
-
 if __name__ == '__main__':
     suite = unittest.TestSuite()
     suite.addTest(Login('test_login1'))
