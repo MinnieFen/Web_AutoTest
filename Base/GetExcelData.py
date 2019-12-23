d1e3ffa6efad344ac180d17085cf41a456809ac7
ddddd <ccc>
2019-12-23 17:51:01 +0800

    修改测试数据存放文件名

--- Base/GetExcelData.py
+++ Base/GetExcelData.py
@@ -5,7 +5,7 @@ import os
 # 调用read_excel方法，直接返回数据列表
 def get_excel_data(sheetname):
     filepath_now = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
-    excel_path = filepath_now + '\data\elementData.xlsx'
+    excel_path = filepath_now + '\TestData\\test_data.xlsx'
     # print(ReadExcel(excel_path,sheetname).read_excel_data())
     return ReadExcel(excel_path,sheetname).read_excel_data()
-# get_excel_data('loginpage')
+# get_excel_data('logindata')
